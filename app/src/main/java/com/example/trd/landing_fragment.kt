48c03ec827780f6e840a5da1c2a97983563053c4
Ld. Nelson <nelsonmwangi197@gmail.com>
2022-03-14 14:28:25 +0300

    homepage started

--- app/src/main/java/com/example/trd/landing_fragment.kt
+++ app/src/main/java/com/example/trd/landing_fragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [login_fragment.newInstance] factory method to
+ * Use the [landing_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class login_fragment : Fragment() {
+class landing_fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,17 +34,26 @@ class login_fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-     return inflater.inflate(R.layout.fragment_login_fragment, container, false)
-
+        return inflater.inflate(R.layout.fragment_landing_fragment, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<TextView>(R.id.register_page_link).setOnClickListener {
-            view.findNavController().navigate(R.id.action_login_fragment_to_register_fragment)
-        }
-
-        view.findViewById<TextView>(R.id.login_button).setOnClickListener {
-            view.findNavController().navigate(R.id.action_login_fragment_to_landing_fragment)
-        }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment landing_fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            landing_fragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
