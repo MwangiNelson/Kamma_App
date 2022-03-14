816d48e8d09bfdc4fa70de5a00fc93c03e393043
Ld. Nelson <nelsonmwangi197@gmail.com>
2022-03-14 11:28:14 +0300

    Monday morning,transitions working(not beautiful tho)

---
+++ app/src/main/java/com/example/trd/login_fragment.kt
@@ -0,0 +1,49 @@
+package com.example.trd
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [login_fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class login_fragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+     return inflater.inflate(R.layout.fragment_login_fragment, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<TextView>(R.id.register_page_link).setOnClickListener {
+            view.findNavController().navigate(R.id.action_login_fragment_to_register_fragment)
+        }
+    }
+}
--- app/src/main/java/com/example/trd/registration_activity.kt
+++ app/src/main/java/com/example/trd/registration_activity.kt
@@ -7,5 +7,6 @@ class registration_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
     }
 }
