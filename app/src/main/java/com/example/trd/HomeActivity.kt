a861ad71b40b29a2e6c1d09f2b64af48e8bfd1cc
Ld. Nelson <67654295+MwangiNelson@users.noreply.github.com>
2022-04-26 23:32:26 +0300

    display stuff

--- app/src/main/java/com/example/trd/HomeActivity.kt
+++ app/src/main/java/com/example/trd/HomeActivity.kt
@@ -1,20 +1,24 @@
 package com.example.trd
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBindings
 import com.example.trd.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_landing.*
-import kotlinx.android.synthetic.main.activity_landing.userEmailLanding
 import kotlinx.android.synthetic.main.nav_header_home.*
+import kotlinx.android.synthetic.main.nav_header_home.view.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -25,19 +29,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val userEmail = intent.getStringExtra("user_email")
-//        userEmailLanding.text = "$userEmail"
-
 
 
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+
 
         setSupportActionBar(binding.appBarHome.toolbar)
 
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
+
+        logout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+
         val navController = findNavController(R.id.nav_host_fragment_content_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
